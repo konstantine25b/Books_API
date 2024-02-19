f5f28368420895cb92a598c47396f857a1d745bc
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-02-19 19:43:00 +0100

    basic setup

---
+++ books/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length= 100)
+    nationality = models.CharField(max_length= 100)
+    
+    def __str__(self):
+        return self.name
+
+class book(models.Model):
+    title = models.CharField(max_length =100)
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+    publication_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
