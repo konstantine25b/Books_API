884ecd11faf1dc8b79d56c1b43eb8e1b555395cd
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-02-20 10:57:08 +0100

    added some tests: book and author tests

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,36 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+class AuthorTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        
+        
+    def test_list_authors(self):
+        response = self.client.get(reverse('author-list'))
+        self.assertEqual(response.status_code , status.HTTP_200_OK)
+        
+    def test_create_author(self):
+        response =self.client.post(reverse('author-list') , {'name': 'kosiko' , 'nationality' : 'kai bichi'},format = 'json')
+        self.assertEqual(response.status_code , status.HTTP_201_CREATED)
+        
+class BookListTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser' , password ='testuser123')
+        self.token = RefreshToken.for_user(self.user)
+        
+    def test_list_books_unauth(self):
+        response = self.client.get(reverse('book-list'))
+        self.assertEqual(response.status_code , status.HTTP_401_UNAUTHORIZED)
+        
+    def test_list_books_auth(self):
+        self.client.credentials(HTTP_AUTHORIZATION = f'Bearer {self.token.access_token}')
+        response = self.client.get(reverse('book-list'))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        
+        
